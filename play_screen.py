8179c7dd43f1d1cfbf1dd750954d5c4f6f9857f7
unknown <israttasnimesha1046@gmail.com>
2024-04-29 04:17:01 +0600

    esha

--- play_screen.py
+++ play_screen.py
@@ -12,7 +12,7 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 GRAY = (200, 200, 200)
 BLUE = (0, 0, 255, 128)
-RED = (255, 0, 0)
+RED = (255, 0, 0, 128)
 
 def generate_random_positions(num_images, existing_positions):
     positions = []
@@ -39,11 +39,11 @@ positions = generate_random_positions(random.randint(2, 8), [])
 
 font = pygame.font.SysFont(None, 20)
 
-attack_choose_button_x_start1 = 20
-attack_choose_button_y_start1 = 600
+attack_choose_button_x_start0 = 20
+attack_choose_button_y_start0 = 600
 
-attack_choose_button_x_start2 = 1260
-attack_choose_button_y_start2 = 600
+attack_choose_button_x_start1 = 1260
+attack_choose_button_y_start1 = 600
 
 attack_choose_button_width, attack_choose_button_height = 105, 50
 attack_choose_button_spacing = 10
@@ -54,9 +54,9 @@ def draw_attack_choose_buttons(window, attack_choose_button_texts, attack_choose
             text = attack_choose_button_texts[i * 2 + j]
             attack_choose_button_rect = pygame.Rect(attack_choose_button_x_start + j * (attack_choose_button_width + attack_choose_button_spacing), attack_choose_button_y_start + i * (attack_choose_button_height + attack_choose_button_spacing), attack_choose_button_width, attack_choose_button_height)
 
-            color = BLUE if current_button_clicked == i * 2 + j else GRAY  # Change color if clicked
+            color = BLUE if current_button_clicked == i * 2 + j else GRAY 
             pygame.draw.rect(window, color, attack_choose_button_rect)
-            pygame.draw.rect(window, BLACK, attack_choose_button_rect, 2)  # Border
+            pygame.draw.rect(window, BLACK, attack_choose_button_rect, 2) 
 
             text_surface = font.render(text, True, BLACK)
             text_rect = text_surface.get_rect(center=attack_choose_button_rect.center)
@@ -70,10 +70,10 @@ def play_screen(toss_result, selected_field):
     background_image = pygame.transform.scale(background_image, WINDOW_SIZE)
 
     running = True
+    current_pokemon_choose_button_clicked0 = None
     current_pokemon_choose_button_clicked1 = None
-    current_pokemon_choose_button_clicked2 = None
+    current_attack_choose_button_clicked0 = None
     current_attack_choose_button_clicked1 = None
-    current_attack_choose_button_clicked2 = None
 
     while running:
         window.blit(background_image, (0, 0))
@@ -88,7 +88,7 @@ def play_screen(toss_result, selected_field):
 
         pokemon_button_images0 = []
         pokemon_button_images1 = []
-        for i in range(0, 3):
+        for i in range(3):
             pokemon_button_image = pygame.image.load(f'Resources/pokemon_{i}.jpeg')
             pokemon_button_image_scaled0 = pygame.transform.scale(pokemon_button_image, (65, 65))
             pokemon_button_images0.append(pokemon_button_image_scaled0)
@@ -97,69 +97,40 @@ def play_screen(toss_result, selected_field):
 
         pokemon_fight_images0 = []
         pokemon_fight_images1 = []
-        for i in range(0, 3):
-            pokemon_fight_image0 = pygame.image.load(f'Resources/pokemon_fight_0_{i}.png')
+        for i in range(3):
+            pokemon_fight_image0 = pygame.image.load(f'Resources/pokemon_fight_{i}_0.png')
             pokemon_fight_image0 = pygame.transform.scale(pokemon_fight_image0, (130, 150))
             pokemon_fight_images0.append(pokemon_fight_image0)
-            pokemon_fight_image1 = pygame.image.load(f'Resources/pokemon_fight_1_{i}.png')
+            pokemon_fight_image1 = pygame.image.load(f'Resources/pokemon_fight_{i}_1.png')
             pokemon_fight_image1 = pygame.transform.scale(pokemon_fight_image1, (130, 150))
             pokemon_fight_images1.append(pokemon_fight_image1)
 
-        #thunder_bolt_attack_button_image1 = pygame.image.load('Resources/thunder_bolt_1.png')
-        #thunder_bolt_attack_button_image1 = pygame.transform.scale(thunder_bolt_attack_button_image1, (50, 50))
-#
-        #thunder_bolt_attack_button_image2 = pygame.image.load('Resources/thunder_bolt_2.png')
-        #thunder_bolt_attack_button_image2 = pygame.transform.scale(thunder_bolt_attack_button_image2, (50, 50))
-#
-        #electro_ball_attack_button_image1 = pygame.image.load('Resources/electro_ball_1.png')
-        #electro_ball_attack_button_image1 = pygame.transform.scale(electro_ball_attack_button_image1, (50, 50))
-#
-        #electro_ball_attack_button_image2 = pygame.image.load('Resources/electro_ball_2.png')
-        #electro_ball_attack_button_image2 = pygame.transform.scale(electro_ball_attack_button_image2, (50, 50))
-#
-        #ember_attack_button_image1 = pygame.image.load('Resources/ember_1.png')
-        #ember_attack_button_image1 = pygame.transform.scale(ember_attack_button_image1, (50, 50))
-#
-        #ember_attack_button_image2 = pygame.image.load('Resources/ember_2.png')
-        #ember_attack_button_image2 = pygame.transform.scale(ember_attack_button_image2, (50, 50))
-#
-        #flamethrower_attack_button_image1 = pygame.image.load('Resources/flamethrower_1.png')
-        #flamethrower_attack_button_image1 = pygame.transform.scale(flamethrower_attack_button_image1, (50, 50))
-#
-        #flamethrower_attack_button_image2 = pygame.image.load('Resources/flamethrower_2.png')
-        #flamethrower_attack_button_image2 = pygame.transform.scale(flamethrower_attack_button_image2, (50, 50))
-#
-        #fire_spin_attack_button_image1 = pygame.image.load('Resources/fire_spin_1.png')
-        #fire_spin_attack_button_image1 = pygame.transform.scale(fire_spin_attack_button_image1, (50, 50))
-#
-        #fire_spin_attack_button_image2 = pygame.image.load('Resources/fire_spin_2.png')
-        #fire_spin_attack_button_image2 = pygame.transform.scale(fire_spin_attack_button_image2, (50, 50))
-#
-        #water_gun_attack_button_image1 = pygame.image.load('Resources/water_gun_1.png')
-        #water_gun_attack_button_image1 = pygame.transform.scale(water_gun_attack_button_image1, (50, 50))
-#
-        #water_gun_attack_button_image2 = pygame.image.load('Resources/water_gun_2.png')
-        #water_gun_attack_button_image2 = pygame.transform.scale(water_gun_attack_button_image2, (50, 50))
-#
-        #bubble_beam_attack_button_image1 = pygame.image.load('Resources/bubble_beam_1.png')
-        #bubble_beam_attack_button_image1 = pygame.transform.scale(bubble_beam_attack_button_image1, (50, 50))
-#
-        #bubble_beam_attack_button_image2 = pygame.image.load('Resources/bubble_beam_2.png')
-        #bubble_beam_attack_button_image2 = pygame.transform.scale(bubble_beam_attack_button_image2, (50, 50))
-#
-        #water_pulse_attack_button_image1 = pygame.image.load('Resources/water_pulse_1.png')
-        #water_pulse_attack_button_image1 = pygame.transform.scale(water_pulse_attack_button_image1, (50, 50))
-#
-        #water_pulse_attack_button_image2 = pygame.image.load('Resources/water_pulse_2.png')
-        #water_pulse_attack_button_image2 = pygame.transform.scale(water_pulse_attack_button_image2, (50, 50))
+        pokemon_attack_images0 = []
+        pokemon_attack_images1 = []
+        missing_files = []
 
-        pokemon_choose_button_x1 = 301
+        for i in range(3):
+            for j in range(4):
+                try:
+                    pokemon_attack_image0 = pygame.image.load(f'Resources/pokemon_{i}_attack_{j}_0.png')
+                    pokemon_attack_image0 = pygame.transform.scale(pokemon_attack_image0, (130, 150))
+                    pokemon_attack_images0.append(pokemon_attack_image0)
+
+                    pokemon_attack_image1 = pygame.image.load(f'Resources/pokemon_{i}_attack_{j}_1.png')
+                    pokemon_attack_image1 = pygame.transform.scale(pokemon_attack_image1, (130, 150))
+                    pokemon_attack_images1.append(pokemon_attack_image1)
+                    
+                    valid_pair = (i, j)
+                except FileNotFoundError:
+                    missing_files.append((i, j))
+
+        pokemon_choose_button_x0 = 301
+        pokemon_choose_button_x0 = [pokemon_choose_button_x0, pokemon_choose_button_x0 + 75, pokemon_choose_button_x0 + 150]
+        pokemon_choose_button_y0 = 45
+
+        pokemon_choose_button_x1 = 983
         pokemon_choose_button_x1 = [pokemon_choose_button_x1, pokemon_choose_button_x1 + 75, pokemon_choose_button_x1 + 150]
         pokemon_choose_button_y1 = 45
-
-        pokemon_choose_button_x2 = 983
-        pokemon_choose_button_x2 = [pokemon_choose_button_x2, pokemon_choose_button_x2 + 75, pokemon_choose_button_x2 + 150]
-        pokemon_choose_button_y2 = 45
 
         if selected_field == 'Aquatic Field':
             field_image = pygame.image.load('Resources/aquatic.png')
@@ -181,47 +152,59 @@ def play_screen(toss_result, selected_field):
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_x, mouse_y = event.pos
                 pokemon_choose_button_width, pokemon_choose_button_height = 70, 70
-                for i, x in enumerate(pokemon_choose_button_x2):
-                    if x <= mouse_x <= x + pokemon_choose_button_width and pokemon_choose_button_y2 <= mouse_y <= pokemon_choose_button_y2 + pokemon_choose_button_height:
-                        current_pokemon_choose_button_clicked2 = i
+                for i, x in enumerate(pokemon_choose_button_x1):
+                    if x <= mouse_x <= x + pokemon_choose_button_width and pokemon_choose_button_y1 <= mouse_y <= pokemon_choose_button_y1 + pokemon_choose_button_height:
+                        current_pokemon_choose_button_clicked1 = i
 
                 for i in range(2):
                     for j in range(2):
-                        button_x = attack_choose_button_x_start2 + j * (attack_choose_button_width + attack_choose_button_spacing)
-                        button_y = attack_choose_button_y_start2 + i * (attack_choose_button_height + attack_choose_button_spacing)
+                        button_x = attack_choose_button_x_start1 + j * (attack_choose_button_width + attack_choose_button_spacing)
+                        button_y = attack_choose_button_y_start1 + i * (attack_choose_button_height + attack_choose_button_spacing)
 
                         if button_x <= mouse_x <= button_x + attack_choose_button_width and button_y <= mouse_y <= button_y + attack_choose_button_height:
-                            current_attack_choose_button_clicked2 = i * 2 + j
-                            print("Clicked button:", current_attack_choose_button_clicked2)
+                            current_attack_choose_button_clicked1 = i * 2 + j
+                            print("Clicked button:", current_attack_choose_button_clicked1)
 
         if selected_field == "Electric Field":
-            current_pokemon_choose_button_clicked1 = 0
+            current_pokemon_choose_button_clicked0 = 0
         if selected_field == "Infernal Field":
-            current_pokemon_choose_button_clicked1 = 1
+            current_pokemon_choose_button_clicked0 = 1
         if selected_field == "Aquatic Field":
-            current_pokemon_choose_button_clicked1 = 2
+            current_pokemon_choose_button_clicked0 = 2
 
-        for x in pokemon_choose_button_x1:
-            pygame.draw.rect(window, (0, 0, 0), (x - 2, pokemon_choose_button_y1 - 2, 70, 70), 2)
+        for x in pokemon_choose_button_x0:
+            pygame.draw.rect(window, (0, 0, 0), (x - 2, pokemon_choose_button_y0 - 2, 70, 70), 2)
 
-        for i in range(0, 3):
-            if current_pokemon_choose_button_clicked1 == i:
-                pokemon_button_images0[i].fill((255, 0, 0, 128), None, pygame.BLEND_RGBA_MULT)
+        for i in range(3):
+            if current_pokemon_choose_button_clicked0 == i:
+                pokemon_button_images0[i].fill(RED, None, pygame.BLEND_RGBA_MULT)
                 window.blit(pokemon_fight_images0[i], (265, 305))
 
-        for i in range(0, 3):
-            window.blit(pokemon_button_images0[i], (pokemon_choose_button_x1[i], pokemon_choose_button_y1))
+        for i in range(3):
+            window.blit(pokemon_button_images0[i], (pokemon_choose_button_x0[i], pokemon_choose_button_y0))
 
-        for x in pokemon_choose_button_x2:
-            pygame.draw.rect(window, (0, 0, 0), (x - 2, pokemon_choose_button_y1 - 2, 70, 70), 2)
+        for x in pokemon_choose_button_x1:
+            pygame.draw.rect(window, (0, 0, 0), (x - 2, pokemon_choose_button_y0 - 2, 70, 70), 2)
 
-        for i in range(0, 3):
-            if current_pokemon_choose_button_clicked2 == i:
-                pokemon_button_images1[i].fill((255, 0, 0, 128), None, pygame.BLEND_RGBA_MULT)
+        for i in range(3):
+            if current_pokemon_choose_button_clicked1 == i:
+                pokemon_button_images1[i].fill(RED, None, pygame.BLEND_RGBA_MULT)
                 window.blit(pokemon_fight_images1[i], (WIDTH-395, 305))
         
-        for i in range(0, 3):
-            window.blit(pokemon_button_images1[i], (pokemon_choose_button_x2[i], pokemon_choose_button_y2))
+        for i in range(3):
+            window.blit(pokemon_button_images1[i], (pokemon_choose_button_x1[i], pokemon_choose_button_y1))
+
+        if current_pokemon_choose_button_clicked0 == 0:
+            attack_choose_button_texts0 = ["Thunderbolt", "Quick Attack", "Electro Ball", "Iron Tail"]
+            draw_attack_choose_buttons(window, attack_choose_button_texts0, attack_choose_button_x_start0, attack_choose_button_y_start0, current_attack_choose_button_clicked0)
+
+        elif current_pokemon_choose_button_clicked0 == 1:
+            attack_choose_button_texts0 = ["Ember", "Flamethrower", "Fire Spin", "Fire Fang"]
+            draw_attack_choose_buttons(window, attack_choose_button_texts0, attack_choose_button_x_start0, attack_choose_button_y_start0, current_attack_choose_button_clicked0)
+
+        elif current_pokemon_choose_button_clicked0 == 2:
+            attack_choose_button_texts0 = ["Water Gun", "Bubble Beam", "Water Pulse", "Aqua Tail"]
+            draw_attack_choose_buttons(window, attack_choose_button_texts0, attack_choose_button_x_start0, attack_choose_button_y_start0, current_attack_choose_button_clicked0)
 
         if current_pokemon_choose_button_clicked1 == 0:
             attack_choose_button_texts1 = ["Thunderbolt", "Quick Attack", "Electro Ball", "Iron Tail"]
@@ -234,22 +217,6 @@ def play_screen(toss_result, selected_field):
         elif current_pokemon_choose_button_clicked1 == 2:
             attack_choose_button_texts1 = ["Water Gun", "Bubble Beam", "Water Pulse", "Aqua Tail"]
             draw_attack_choose_buttons(window, attack_choose_button_texts1, attack_choose_button_x_start1, attack_choose_button_y_start1, current_attack_choose_button_clicked1)
-
-        if current_pokemon_choose_button_clicked2 == 0:
-            attack_choose_button_texts2 = ["Thunderbolt", "Quick Attack", "Electro Ball", "Iron Tail"]
-            draw_attack_choose_buttons(window, attack_choose_button_texts2, attack_choose_button_x_start2, attack_choose_button_y_start2, current_attack_choose_button_clicked2)
-
-        elif current_pokemon_choose_button_clicked2 == 1:
-            attack_choose_button_texts2 = ["Ember", "Flamethrower", "Fire Spin", "Fire Fang"]
-            draw_attack_choose_buttons(window, attack_choose_button_texts2, attack_choose_button_x_start2, attack_choose_button_y_start2, current_attack_choose_button_clicked2)
-
-        elif current_pokemon_choose_button_clicked2 == 2:
-            attack_choose_button_texts2 = ["Water Gun", "Bubble Beam", "Water Pulse", "Aqua Tail"]
-            draw_attack_choose_buttons(window, attack_choose_button_texts2, attack_choose_button_x_start2, attack_choose_button_y_start2, current_attack_choose_button_clicked2)
-
-        if current_attack_choose_button_clicked2 == 0:
-            pikachu_button_image2.fill((0, 0, 255, 128), None, pygame.BLEND_RGBA_MULT)
-            window.blit(pikachu_fight_image2, (1105, 305))
 
         pygame.display.flip()
 
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,7 +1,1 @@
-or j in range(2):
-                        button_x = attack_choose_button_x_start2 + j * (attack_choose_button_width + attack_choose_button_spacing)
-                        button_y = attack_choose_button_y_start2 + i * (attack_choose_button_height + attack_choose_button_spacing)
-
-                        if button_x <= mouse_x <= button_x + attack_choose_button_width and button_y <= mouse_y <= button_y + attack_choose_button_height:
-                            current_attack_choose_button_clicked2 = i * 2 + j
-                            print("Clicked button:", current_attack_choose_button_clicked2)
+2
